57274cbcec294c2ded0fe95e7180b42f084cdcda
Swaroop <65998110+Swaroop-dev@users.noreply.github.com>
2023-11-22 12:29:44 +0530

    Removed  unnecessary comments

--- schema/schema.sql
+++ schema/schema.sql
@@ -2,6 +2,3 @@ CREATE TABLE IF NOT EXISTS movies (id VARCHAR(255),title VARCHAR(255),descriptio
 
 CREATE TABLE IF NOT EXISTS ratings (record_id VARCHAR(255),rating_type VARCHAR(255),user_id VARCHAR(255),value INT);
 
--- docker exec -i movieexample_db1 mysql movieexample -h localhost -P 3306 --protocol=tcp -uroot -ppassword < schema/schema.sql
-
---docker exec -i movieexample_db1 mysql movieexample -h localhost -P 3306 --protocol=tcp -uroot -ppassword -e "SHOW tables"
